040aefc227b0925988696081808f21e4d3f9a697
uswa.r <uswa.r@outlook.com>
2022-09-28 15:01:24 +0500

    x

--- src/app/main_compo/type-orm/type-orm.component.ts
+++ src/app/main_compo/type-orm/type-orm.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { createConnection, Connection, CustomRepositoryCannotInheritRepositoryError } from 'typeorm';
 
 @Component({
   selector: 'app-type-orm',
@@ -12,8 +11,3 @@ export class TypeOrmComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-
-const myUser = await respository.findOne({where: {name:"Troy"}});
-await respository.delete(mymUser.id);
-}
